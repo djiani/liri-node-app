65e3efe18209be3b51839b7d56215dcc4e10fd33
Uncle Sam <djiasara@gmail.com>
2019-05-14 09:41:18 -0400

    implementing movieThis function which use axios to call OMDB api

--- liri.js
+++ liri.js
@@ -1,18 +1,17 @@
 require("dotenv").config();
+const axios = require('axios');
+
 const Spotify = require('node-spotify-api');
-
-
 const keys = require("./keys.js");
 const spotify = new Spotify(keys.spotify);
 
+//global variable
+const OMDB_BASEURL= "http://www.omdbapi.com/";
 
-
-
-//console.log(keys);
-
+//get command line argument
 const whatToDo = process.argv[2];
 const searchArg = process.argv.slice(3).join("+");
-console.log(searchArg);
+//console.log(searchArg);
 
 function spotifyThisSong(trackName) {
     spotify
@@ -33,8 +32,32 @@ function spotifyThisSong(trackName) {
         });
 }
 
+function movieThis(title){
+    axios.get(OMDB_BASEURL,{
+        params:{
+            apikey: 'trilogy',
+            t: title ||"Mr. Nobody"
+        }
+    } ).then(function(response){
+        console.log(`
+        Title: ${response.data.Title}
+        Year: ${response.data.Year}
+        IMDB rating: ${response.data.imdbRating}
+        Rotten Tomatoes Rating: ${response.data.Ratings[1].Value}
+        Country of Production: ${response.data.Country}
+        Language of the movies: ${response.data.Language}
+        Plot: ${response.data.Plot}
+        Actors: ${response.data.Actors}
+        `)
+    }).catch(function(err){
+        console.log(err);
+    })
+    
+}
 switch (whatToDo) {
-    case 'spotify-this-song': spotifyThisSong();
+    case 'spotify-this-song': spotifyThisSong(searchArg);
+        break;
+    case 'movie-this': movieThis(searchArg);
         break;
     default:
         console.log("sorry, can find this command! Enter a valid command!");
